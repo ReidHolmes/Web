e39ef5a6462e1325cb36ddf54422cd88f5c6deae
ReidHolmes <name.reidholmes@gmail.com>
2019-02-23 20:31:35 +0800

    有点头目

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,23 +6,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*  <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>*/}
-          {/*<a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>*/}
-        </header>
-      </div>
+          <button className="btn">Add</button>
+          <ul className="list-group">
+            <li className="list-group-item">Cras justo odio</li>
+            <li className="list-group-item">Dapibus ac facilisis in</li>
+            <li className="list-group-item">Morbi leo risus</li>
+            <li className="list-group-item">Porta ac consectetur ac</li>
+            <li className="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </div>
+
     );
   }
 }
+
+
+
+
+
+
 
 export default App;
